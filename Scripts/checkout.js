d8dbcdca74551063ff2326d8babd66dd6165349c
Vinayak Bhodekar <bhodekarvinayak71@gmail.com>
2024-10-05 18:59:51 +0530

    Update delete button , delete product for cart when click delete

--- Scripts/checkout.js
+++ Scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart,removefromcart} from '../data/cart.js';
 import {product} from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 let cartSummaryHTML = '';
@@ -38,7 +38,7 @@ cart.forEach((cartitem) => {
             <span class="update-quantity-link link-primary">
               Update
             </span>
-            <span class="delete-quantity-link link-primary">
+            <span class="delete-quantity-link link-primary js-delete-link"  data-product-id = "${matchingproduct.id}">
               Delete
             </span>
           </div>
@@ -93,3 +93,11 @@ cart.forEach((cartitem) => {
   `;
 });
 document.querySelector(".js-order-summary").innerHTML = cartSummaryHTML;
+
+document.querySelectorAll(".js-delete-link").forEach((link) => {
+  link.addEventListener('click',() => {
+    const productId = link.dataset.productId; 
+    removefromcart(productId);
+    console.log(cart);
+  });
+});
