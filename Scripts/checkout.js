5fdc14b6e143c022e8a686113007340765e34ed6
Vinayak Bhodekar <bhodekarvinayak71@gmail.com>
2024-10-26 11:44:06 +0530

    learn promises and convert code to promises

--- Scripts/checkout.js
+++ Scripts/checkout.js
@@ -3,13 +3,47 @@ import { renderpaymentsummary } from './Checkouts/paymentSummary.js';
 import {renderCheckoutHeader} from './Checkouts/CheckoutHeader.js'
 //import '../data/car.js';
 //import '../data/cart-classes.js';
-import '../data/backend-practice.js';
+//import '../data/backend-practice.js';
 import { loadProducts } from '../data/products.js';
+import { loadCart } from '../data/cart.js';
 
-
-loadProducts(() => {
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(() => {
+      resolve('Values1');
+    });
+  }),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  })
+]).then((values) => {
+  console.log(values);
   renderCheckoutHeader();
   renderpaymentsummary();
   renderOrderSummary();
 });
 
+new Promise((resolve) => {
+  loadProducts(() => {
+    resolve();
+  });
+}).then((values) => {
+  return new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  });
+}).then(() => {
+  renderCheckoutHeader();
+  renderpaymentsummary();
+  renderOrderSummary();
+});
+
+/*loadProducts(() => {
+  renderCheckoutHeader();
+  renderpaymentsummary();
+  renderOrderSummary();
+});
+*/
--- data/cart.js
+++ data/cart.js
@@ -102,3 +102,13 @@ export function getcart_product(productID){
     });  
   return matchingproduct;
 }
+
+export function loadCart (fun){
+  const xhr = new XMLHttpRequest();
+  xhr.addEventListener('load', () => {
+    console.log(xhr.response);
+    fun();
+  });
+  xhr.open('GET', 'https://supersimplebackend.dev/cart');
+  xhr.send();
+}
